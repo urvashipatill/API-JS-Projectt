a367d837d8f4b3c3c96040cbef62de192b29b69b
urvashipatill <urvashimurlipatil23@gmail.com>
2023-12-02 00:28:34 +0530

    promise changed to snyc await

--- project.js
+++ project.js
@@ -1,73 +1,77 @@
- function addProduct(event){
-    event.preventDefault()
-    const pricevalue= event.target.price.value
-    const productype=event.target.name.value
-    const categoryvalue=event.target.category.value
+// on submit button event
+async function addProduct(event) {
+  try {
+    event.preventDefault();
+    const pricevalue = event.target.price.value;
+    const productype = event.target.name.value;
+    const categoryvalue = event.target.category.value;
 
-    const obj={
-        pricevalue,
-        productype,
-        categoryvalue
+    const obj = {
+      pricevalue,
+      productype,
+      categoryvalue,
+    };
+    const response = await axios.post(
+      "https://crudcrud.com/api/125022e5549e432fa6de1755339ac25d/savedata",
+      obj
+    );
+
+    showOnscreen(response.data);
+    document.getElementById("price").value = "";
+    document.getElementById("name").value = "";
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//  Store back data on screen
+async function get() {
+  try {
+    const response = await axios.get(
+      "https://crudcrud.com/api/125022e5549e432fa6de1755339ac25d/savedata"
+    );
+
+    for (let i = 0; i < response.data.length; i++) {
+      showOnscreen(response.data[i]);
     }
-    axios.post('https://crudcrud.com/api/c22f1d91abed4ee790c0ce6dde547b22/savedata',obj)
-    .then((response) =>{
-    showOnscreen(response.data)
-    })
-    .catch((err) =>{
-        console.log(err)
-    })
-    document.getElementById('price').value=''
-    document.getElementById('name').value=''
- }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+function showOnscreen(e) {
+  if (e.categoryvalue === "Men") {
+    let parentNode = document.getElementById("mensection");
+    let childHTML = `<li id=${e._id}> Price:${e.pricevalue} - Product:${e.productype} - Category:${e.categoryvalue}
+         <button onclick="deleteProduct('${e._id}')">Delete</button> `;
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+  } else if (e.categoryvalue === "Women") {
+    let parentNode = document.getElementById("womensection");
+    let childHTML = `<li id=${e._id}> Price:${e.pricevalue} - Product:${e.productype} - Category:${e.categoryvalue}
+            <button onclick="deleteProduct('${e._id}')">Delete</button> `;
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+  } else if (e.categoryvalue === "Kids") {
+    let parentNode = document.getElementById("kidsection");
+    let childHTML = `<li id=${e._id}> Price:${e.pricevalue} - Product:${e.productype} - Category:${e.categoryvalue}
+           <button onclick="deleteProduct('${e._id}')">Delete</button> `;
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+  }
+}
 
- axios.get('https://crudcrud.com/api/c22f1d91abed4ee790c0ce6dde547b22/savedata')
- .then((response) =>{
-    for(let i=0; i<response.data.length ;i++){
-        showOnscreen(response.data[i])
-    }
- })
- .catch((err) =>{
-    console.log(err)
- })
-
-
- function showOnscreen(e){
-  
-    if(e.categoryvalue==='Men'){
-        let parentNode= document.getElementById('mensection')
-         let childHTML=`<li id=${e._id}> Price:${e.pricevalue} - Product:${e.productype} - Category:${e.categoryvalue}
-         <button onclick="deleteProduct('${e._id}')">Delete</button> `
-        parentNode.innerHTML= parentNode.innerHTML+ childHTML
-        }
-         else if(e.categoryvalue==='Women'){
-            let parentNode= document.getElementById('womensection')
-            let childHTML=`<li id=${e._id}> Price:${e.pricevalue} - Product:${e.productype} - Category:${e.categoryvalue}
-            <button onclick="deleteProduct('${e._id}')">Delete</button> `
-           parentNode.innerHTML= parentNode.innerHTML+ childHTML
-        }
-        else if(e.categoryvalue==='Kids'){
-            let parentNode= document.getElementById('kidsection')
-            let childHTML=`<li id=${e._id}> Price:${e.pricevalue} - Product:${e.productype} - Category:${e.categoryvalue}
-           <button onclick="deleteProduct('${e._id}')">Delete</button> `
-           parentNode.innerHTML= parentNode.innerHTML+ childHTML
-        }
- }
-
- function deleteProduct(id){
-    confirm('Do you want to delete the product?')
+//    removing product
+async function deleteProduct(id) {
+  try {
+    confirm("Do you want to delete the product?");
     {
-        axios.delete(`https://crudcrud.com/api/c22f1d91abed4ee790c0ce6dde547b22/savedata/${id}`)
-        .then((response) =>{
-         let child=document.getElementById(id)
-         let parentNode= child.parentElement
-         parentNode.removeChild(child)
-    
-        })
-        .catch((err) =>{
-            console.log(err)
-        })
-    }
-   
- }
+      const response = await axios.delete(
+        `https://crudcrud.com/api/125022e5549e432fa6de1755339ac25d/savedata/${id}`
+      );
 
+      let child = document.getElementById(id);
+      let parentNode = child.parentElement;
+      parentNode.removeChild(child);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
